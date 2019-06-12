538abc4cf552edb149c7ad3c698c5040e9b1e47e
Nick <849003654@qq.com>
2019-06-12 22:32:27 +0800

    删除target

--- spring-demo-mana/spring-demo-mana-web/target/spring-demo-mana-web/scripts/layout.js
+++
@@ -1,3 +0,0 @@
-define('layout',['jquery'],function ($) {
-
-})
--- spring-demo-mana/spring-demo-mana-web/target/spring-demo-mana-web/scripts/plugins/layui/layui.init.js
+++
@@ -1,4 +0,0 @@
-define(['layui'], function () {
-    layui.config({dir: requirejs.s.contexts._.config.baseUrl + 'plugins/layui/'});
-    return {layui:layui}
-})
